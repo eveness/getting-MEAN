051fb879606726d76ab0c0aaedfd7be2f83c3ec6
eveness <eveness@gmail.com>
2017-03-13 01:13:07 +0300

    heroku fix

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ require('./app_api/models/db');
 var uglifyJs = require("uglify-js");
 var fs = require('fs');
 
-//var routes = require('./app_server/routes/index');
+var routes = require('./app_server/routes/index');
 var routesApi = require('./app_api/routes/index');
+// var users = require('./app_server/routes/users');
 
 var app = express();
 
@@ -20,17 +21,17 @@ app.set('view engine', 'jade');
 var appClientFiles = [
   'app_client/app.js',
   'app_client/home/home.controller.js',
-  'app_client/about/about.controller.js',,
-  'app_client/locationDetail/locationDetail.controller.js',,
+  'app_client/about/about.controller.js',
+  'app_client/locationDetail/locationDetail.controller.js',
   'app_client/reviewModal/reviewModal.controller.js',
   'app_client/common/services/geolocation.service.js',
   'app_client/common/services/loc8rData.service.js',
   'app_client/common/filters/formatDistance.filter.js',
   'app_client/common/filters/addHtmlLineBreaks.filter.js',
-  'app_client/common/directives/ratingStars/ratingStars.directive.js',
   'app_client/common/directives/navigation/navigation.directive.js',
   'app_client/common/directives/footerGeneric/footerGeneric.directive.js',
   'app_client/common/directives/pageHeader/pageHeader.directive.js',
+  'app_client/common/directives/ratingStars/ratingStars.directive.js'
 ];
 var uglified = uglifyJs.minify(appClientFiles, { compress : false });
 
@@ -43,7 +44,7 @@ fs.writeFile('public/angular/loc8r.min.js', uglified.code, function (err){
 });
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -51,7 +52,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_client')));
 
-//app.use('/', routes);
+// app.use('/', routes);
 app.use('/api', routesApi);
 
 app.use(function(req, res) {
@@ -60,20 +61,34 @@ app.use(function(req, res) {
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handler
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
+
 
 module.exports = app;
