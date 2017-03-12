2d1e1c589fb0f7fd159e299c4ed562f45b12e631
eveness <eveness@gmail.com>
2017-03-13 00:56:36 +0300

    глава 10, Angular SPA, завершение

--- app_client/common/services/loc8rData.service.js
+++ app_client/common/services/loc8rData.service.js
@@ -10,8 +10,19 @@
       //return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=20');
       return $http.get('/api/locations?lng=30.317812&lat=59.957191&maxDistance=20');
     };
+
+    var locationById = function (locationid) {
+      return $http.get('/api/locations/' + locationid);
+    };
+
+    var addReviewById = function (locationid, data) {
+      return $http.post('/api/locations/' + locationid + '/reviews', data);
+    };
+
     return {
-      locationByCoords : locationByCoords
+      locationByCoords : locationByCoords,
+      locationById : locationById,
+      addReviewById: addReviewById
     };
   }
 
